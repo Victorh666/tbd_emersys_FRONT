9a5f231c1c16c4214e11735dffcc2c9a7674429b
Victor <victor.huanqui@usach.cl>
2020-05-26 05:56:18 -0400

    Project Creation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-TBD_Front
+# tbd_emersys_front
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="p-3">
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        
+
+        <b-navbar-brand :to="{name: 'Dashboard'}">Sistema de Emergencia</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item :to="{name: 'About'}" >About</b-nav-item>
+            
+            <b-nav-item :to="{name: 'Home'}">Home</b-nav-item>
+
+            <b-nav-item-dropdown text="Emergencias" right>
+              <b-dropdown-item href="#">Crear emergencia</b-dropdown-item>
+              <b-dropdown-item href="#">Ver emergencias</b-dropdown-item>
+            </b-nav-item-dropdown>
+            
+            <b-nav-item to="">Usuarios</b-nav-item>
+
+            <b-nav-item to="">Iniciar Sesion</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+
+    <b-container align="center">
+      <router-view/>
+    </b-container>
+  </div>
+</template>
+
