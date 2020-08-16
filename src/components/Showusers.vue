46279766c6ab03929171d8a7ac149b72e291e910
Benja <benjamin.morales@usach.cl>
2020-08-16 15:19:54 -0400

    Added voluntaries filter by ranking

--- src/components/Showusers.vue
+++ src/components/Showusers.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
         <b-row>
-            <b-col v-for="user in userList" :key="user.id"
+            <b-col v-for="vol in filteredVoluntariesRender" :key="vol.id"
                 class="mb-3" cols="12" md="6" lg="4">
                 <b-card no-body style="max-width: 20rem;" img-src="https://placekitten.com/380/200" img-alt="Image" img-top>
                     <b-card-body>
-                        <b-card-title>{{user.name}}</b-card-title>
+                        <b-card-title>{{vol.nombre}}</b-card-title>
                         <b-card-sub-title class="mb-2">Ocupacion</b-card-sub-title>
                         <b-card-text>Descripcion... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis at quos veritatis rerum eum? Quasi eos repudiandae iste numquam laborum, cum, saepe voluptatibus obcaecati nobis eum doloremque quo aspernatur temporibus!</b-card-text>
                     </b-card-body>
 
                     <b-list-group flush>
                         <b-list-group-item class="d-flex justify-content-between align-items-center">
-                            Ranking {{user.ranking}}
-                            <b-form-rating id="rating-disabled" color="#ff8800" inline :value="user.ranking" no-border readonly></b-form-rating>                            
+                            Ranking {{vol.id}}
+                            <b-form-rating id="rating-disabled" color="#ff8800" inline :value="vol.id" no-border readonly></b-form-rating>                            
                         
                         </b-list-group-item>
                         <b-list-group-item class="d-flex justify-content-between align-items-center">
@@ -41,7 +41,7 @@
 export default {
   name: 'Showusers',
   props: {
-    userList: Array
+    filteredVoluntariesRender : null
   }
 }
 </script>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="Users">
+    <div class="users">
         <b-form-select v-model="optionSelected" :options="searchOptions" class="mb-3"></b-form-select>
         <b-form-input v-if="optionSelected === 1"
         class="mb-3" v-model="text" placeholder="Ingrese el nombre"></b-form-input>
@@ -18,7 +18,7 @@
                     <b-form-spinbutton v-model="valueR2" type="range" min="0" max="5" step="0.5" inline></b-form-spinbutton>
                 </b-col>
                 <b-col>
-                    <b-button block variant="outline-info" size="sm" @click="usersFilter(valueR1,valueR2)">Buscar</b-button>
+                    <b-button block variant="outline-info" size="sm" @click="usersFilter(valueR1, valueR2)">Buscar</b-button>
                 </b-col>
             </b-row>
         </b-card>
@@ -27,15 +27,16 @@
         <b-form-input v-if="optionSelected === 4"
         class="mb-3" v-model="text" placeholder="Ingrese la emergencia"></b-form-input>
 
-        <Showusers v-if="filter === 0" :userList="users"/> 
-        <Showusers v-else :userList="newUserList"/> 
+        <Showusers v-if="filter === 0" :filteredVoluntariesRender="voluntaries"/>
+        <Showusers v-else :filteredVoluntariesRender="filteredVoluntaries"/>
+       
     </div>
 </template>
 
 
 <script>
 import Showusers from '../components/Showusers.vue'
-import {mapState} from 'vuex';
+import axios from "axios";
 
 export default {
     name: 'Users',
@@ -57,34 +58,57 @@ export default {
           { value: 3, text: 'Buscar por habilidades' },
           { value: 4, text: 'Buscar por emergencia'}
         ],
-        newUserList: [],
+        voluntariesList: [],
+        voluntaries: null,
+        filteredVoluntaries: null,
+        fields: [{key: 'nombre', label: 'Nombre', sortable: true}, {key: 'fnacimiento', label: 'Fnacimiento'}], 
         filter: 0
       }
     },
-
-    computed: {
-        ...mapState(['users'])
+    created(){
+        this.getVoluntaries();
     },
-
     methods:{
-        usersFilter(value1, value2){
+        async usersFilter(value1, value2){
+            const stringV1 = value1.toString();
+            const stringV2 = value2.toString();
+            const queryString = "http://localhost:8080/voluntarios/getVolWithRankingIn/" + stringV1 + "/and/" + stringV2;
             this.filter = 1;
-            this.newUserList = [];
-            const auxV = value2;
-            if(value1 > value2){
-                value2 = value1
-                value1 = auxV;
+            let filterData = await axios.get(queryString).catch(error=>console.log(error))
+            this.filteredVoluntaries = await filterData.data;
+            console.log(this.filteredVoluntaries.data);
+
+            this.voluntariesList = [];
+            let voluntariesLen = this.filteredVoluntaries.lenght;
+            let i = 0;
+            while(i < voluntariesLen){
+                this.voluntariesList.push({
+                    nombre: this.filteredVoluntaries[i].nombre,
+                    fnacimiento: this.filteredVoluntaries[i].fnacimiento,
+                    id: this.filteredVoluntaries[i].id
+                });
             }
-
-            this.users.forEach(element => {
-                if(element.ranking >= value1 && element.ranking <= value2){
-                    this.newUserList.push(element);
-                }
-            });
-
-            return this.newUserList
-
+            console.log("Assigned filtered voluntaries");
+        },
+        async getVoluntaries(){            
+            let voluntariesData = await axios.get("http://localhost:8080/voluntarios/all")
+            this.voluntaries = await voluntariesData.data;
+            console.log(this.voluntaries.data);
+            this.filter = 0;
+            this.voluntariesList = [];
+            let voluntariesLen = this.voluntaries.length;
+            let i = 0;
+            console.log("i = ", i, ", lenght of voluntaries array = ", voluntariesLen);
+            while(i < voluntariesLen){
+                this.voluntariesList.push({
+                    nombre: this.voluntaries[i].nombre,
+                    fnacimiento: this.voluntaries[i].fnacimiento,
+                    id: this.voluntaries[i].id
+                });
+                i = i + 1;
+            }
+            console.log("Assigned to array");
         }
     }
-}
+};
 </script>
